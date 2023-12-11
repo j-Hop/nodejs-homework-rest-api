908c68aa3cf7f777614d7536b2f09674183a177c
j-Hop <119629763+j-Hop@users.noreply.github.com>
2023-12-11 13:33:32 +0200

    finish

--- controllers/contacts-controllers.js
+++ controllers/contacts-controllers.js
@@ -1,22 +1,11 @@
-import Joi from "joi";
-import contactsService from "../models/contacts.js";
 import { HttpError } from "../helpers/index.js";
+import Contact from "../models/Contact.js"
 
-const contactAddSchema = Joi.object({
-  name: Joi.string().required(),
-  email: Joi.string().email().required(),
-  phone: Joi.string().required(),
-});
 
-const contactUpdateShema = Joi.object({
-  name: Joi.string(),
-  email: Joi.string().email(),
-  phone: Joi.string(),
-});
 
 const getAllContacts = async (req, res, next) => {
     try {
-      const result = await contactsService.listContacts();
+      const result = await Contact.find();
       res.json(result);
     } catch (error) {
       next(error);
@@ -26,7 +15,7 @@ const getAllContacts = async (req, res, next) => {
   const getById = async (req, res, next) => {
     try {
       const { contactId } = req.params;
-      const result = await contactsService.getContactById(contactId);
+      const result = await Contact.findById(contactId);
       if (!result) {
         throw HttpError(404, `Not found`);
       }
@@ -37,52 +26,28 @@ const getAllContacts = async (req, res, next) => {
   };
 
   const deleteContactId = async (req,res, next)=>{
-    try{
        const {contactId} = req.params;
-       const result = await contactsService.removeContact(contactId);
+       const result = await Contact.findByIdAndDelete(contactId);
        if (!result) {
         throw HttpError(404, `Not found`);
       }
     res.json({
         message: "contact deleted"
     })
-    }
-    catch(error){
-      next(error);
-    }
   }
   
   const add = async (req, res, next) => {
-    try {
-      const { error } = contactAddSchema.validate(req.body);
-      if (error) {
-          console.log('error', error)
-        throw HttpError(400, error.message);
-      }
-      const result = await contactsService.addContact(req.body);
+      const result = await Contact.create(req.body);
       res.status(201).json(result);
-    } catch (error) {
-      next(error);
-    }
   };
 
   const updateContacts = async (req, res, next) => {
-    try {
-      const { error } = contactUpdateShema.validate(req.body);
-  
-      if (error) {
-          console.log('error', error)
-        throw HttpError(400, error.message);
-      }
       const {contactId} = req.params;
-      const result = await contactsService.updateContact(contactId, req.body)
+      const result = await Contact.findByIdAndUpdate(contactId, req.body);
       if (!result) {
           throw HttpError(404, `Not found`);
         }
         res.json(result)
-    } catch (error) {
-      next(error);
-    }
   };
 
 
