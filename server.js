c57d6228fa5176c0d9e83c926aa97c8ae1d436b3
j-Hop <119629763+j-Hop@users.noreply.github.com>
2023-12-11 03:06:30 +0200

    up

--- server.js
+++ server.js
@@ -1,5 +1,16 @@
+import mongoose from "mongoose";
+
+
 import app from "./app.js";
 
-app.listen(3000, () => {
-  console.log("Server running. Use our API on port: 3000")
+const DB_HOST = "mongodb+srv://Bogdan:BSM5YC58W9XcpnT6@cluster0.o4cmefp.mongodb.net/db-contacts?retryWrites=true&w=majority";
+
+mongoose.connect(DB_HOST)
+.then(()=> {
+  app.listen(3000, () => {
+    console.log("Server running. Use our API on port: 3000")
+  })
 })
+.catch(error => console.log(error.message))
+
+// BSM5YC58W9XcpnT6
