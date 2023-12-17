3b17b80272c0329153b13406ee77e23f04b2895f
j-Hop <119629763+j-Hop@users.noreply.github.com>
2023-12-17 20:30:08 +0200

    update

--- decorators/crtlWrapper.js
+++ decorators/crtlWrapper.js
@@ -1,12 +1,11 @@
 const ctrlWrapper = (ctrl) => {
-    const func = async (req, res, next) => {
-      try {
-        await ctrl(req, res, next);
-      } catch (error) {
-        next(error);
-      }
-    };
-    return func;
+  return async (req, res, next) => {
+    try {
+      await ctrl(req, res, next);
+    } catch (error) {
+      next(error);
+    }
+  };
   };
   
   export default ctrlWrapper;
